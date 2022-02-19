9d28e33d9399a77e2e41afe670707d9006bf79f9
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2022-02-18 21:27:28 -0800

    Created `/set category` command

--- clovis/commands.py
+++ clovis/commands.py
@@ -1,5 +1,42 @@
+from discord.commands import Option
 from discord.ext import commands
+from . import sessionmaker
+from .tables import Guild
+
+import discord
 
 
 class CommandsCog(commands.Cog):
-    pass
+    set_commands = discord.SlashCommandGroup(
+        'set',
+        'Commands used to configure various aspects of the bot.'
+    )
+
+    @set_commands.command(
+        name='category',
+        description="Use this command to set the category in which new channels will be created in.",
+        options=[
+            Option(
+                discord.CategoryChannel,
+                name='channel',
+                description="The category to create new channels in."
+            )
+        ]
+
+    )
+    @commands.has_guild_permissions(administrator=True)
+    async def set_category(self, ctx: discord.ApplicationContext, channel: discord.CategoryChannel):
+        # NOTE: Still need to do more testing to determine which combination
+        # of permissions will always guarantee that the bot will be able to
+        # create the given channel in the category.
+        if channel.permissions_for(ctx.me).manage_channels:
+            async with sessionmaker.begin() as session:
+                sql_guild = await Guild.get_or_create(session, ctx.guild_id)
+                sql_guild.category_id = channel.id
+                sql_guild.create_channel = True
+            await ctx.respond(f"{channel.mention} has been set as the new category to create private channels in.")
+        else:
+            await ctx.respond(
+                "I don't have access to create channels in this category. "
+                "Please give me access, before setting it as the category."
+            )
