78dec1b838e9f6a1ea451f9df2a137fc7c2851a5
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2022-02-18 21:25:53 -0800

    Created sessionmaker

--- clovis/__init__.py
+++ clovis/__init__.py
@@ -1,3 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker as maker
 
 engine = create_async_engine('sqlite+aiosqlite:///:memory:')
+
+sessionmaker = maker(bind=engine, class_=AsyncSession)
