d38a2db067bcfe49b3875f9efed7a13804bfd1b0
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2022-02-18 21:17:16 -0800

    Created Guild declarative mapping

---
+++ clovis/tables.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, BigInteger, Boolean
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm.decl_api import registry
+from dataclasses import dataclass, field
+
+mapper = registry()
+
+
+@mapper.mapped
+@dataclass
+class Guild:
+    __tablename__ = 'guilds'
+
+    __sa_dataclass_metadata_key__ = 'sa'
+
+    id: int = field(metadata={
+        'sa': Column(BigInteger, primary_key=True)
+        }
+    )
+    category_id: int = field(default=None, metadata={'sa': Column(BigInteger)})
+    last_message_id: int = field(default=None, metadata={'sa': Column(BigInteger)})
+    create_channel: bool = field(default=True, metadata={'sa': Column(Boolean, nullable=False)})
+    message_error: bool = field(default=False, metadata={'sa': Column(Boolean, nullable=False)})
