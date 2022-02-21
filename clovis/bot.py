37b71117e0242e3305aad408a53996b1019ab7ad
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2022-02-20 22:08:45 -0800

    Use config options to determine whether to create guild or global slash commands

--- clovis/bot.py
+++ clovis/bot.py
@@ -1,10 +1,12 @@
 from .commands import CommandsCog
+from . import config
 
 import discord
 
 bot = discord.Bot(
-    debug_guilds=[810742455745773579, 940714521335578634],
+    debug_guilds=[810742455745773579] if config.testing else None,
     intents=discord.Intents(guilds=True, members=True)
 )
-# bot = discord.Bot(intents=discord.Intents(guilds=True, members=True))
 bot.add_cog(CommandsCog())
+
+from .automated import *
