1ed3b83e2134a51e40d7e4296eb1277550736e04
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2022-02-20 22:06:21 -0800

    Added support for command line arguments
    (used to determine if the bot is in testing mode or production mode)

--- clovis/__init__.py
+++ clovis/__init__.py
@@ -1,6 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker as maker
 
-engine = create_async_engine('sqlite+aiosqlite:///:memory:')
+import argparse
+
+parser = argparse.ArgumentParser(description='Use this to set bot settings.')
+parser.add_argument('-nt', '--not-testing', action='store_false', dest='testing')
+config = parser.parse_args()
+
+engine = create_async_engine(f"sqlite+aiosqlite:///{'test.db' if config.testing else 'bot.db'}")
 
 sessionmaker = maker(bind=engine, class_=AsyncSession)
