a928a8dcbd30d45cb701a94f2e6cdd58172b95a4
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2022-03-04 18:10:18 -0800

    Only handle errors in this listener if the error is from this cog

--- clovis/commands.py
+++ clovis/commands.py
@@ -270,22 +270,23 @@ class CommandsCog(commands.Cog):
         ctx: discord.ApplicationContext,
         error: discord.ApplicationCommandInvokeError
     ):
-        if isinstance(error.original, commands.MissingPermissions):
-            logger.info(
-                f"{ctx.author} tried to use the /{ctx.command.qualified_name} "
-                "even though they don't have permission to do so."
-            )
-            await ctx.respond("You do not have permission to use this command.")
-        elif isinstance(error.original, commands.NoPrivateMessage):
-            logger.info(
-                f"{ctx.author} tried to use the /{ctx.command.qualified_name} in a DM."
-            )
-            await ctx.respond("This command is not available in DM messages.")
-        elif isinstance(error.original, commands.BadArgument):
-            # NOTE: We are assuming that if this happens the individual command will have
-            # logic to handle this situation.
-            pass
-        else:
-            # NOTE: In this case you may want to add ping_dev function like with the bdaybot
-            logger.error("The following error occured with the bot:", exc_info=error)
-            await ctx.respond("Uh oh! Something went wrong on our end. Please try again later!")
+        if ctx.cog and ctx.cog is self:
+            if isinstance(error.original, commands.MissingPermissions):
+                logger.info(
+                    f"{ctx.author} tried to use the /{ctx.command.qualified_name} "
+                    "even though they don't have permission to do so."
+                )
+                await ctx.respond("You do not have permission to use this command.")
+            elif isinstance(error.original, commands.NoPrivateMessage):
+                logger.info(
+                    f"{ctx.author} tried to use the /{ctx.command.qualified_name} in a DM."
+                )
+                await ctx.respond("This command is not available in DM messages.")
+            elif isinstance(error.original, commands.BadArgument):
+                # NOTE: We are assuming that if this happens the individual command will have
+                # logic to handle this situation.
+                pass
+            else:
+                # NOTE: In this case you may want to add ping_dev function like with the bdaybot
+                logger.error("The following error occured with the bot:", exc_info=error)
+                await ctx.respond("Uh oh! Something went wrong on our end. Please try again later!")
