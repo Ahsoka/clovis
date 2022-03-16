e7008d72e15559769a65f0cf4c168f9205dcd7d8
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2022-03-15 22:40:53 -0700

    Added `welcome_channel_id` to the Guild declarative mapping

--- clovis/tables.py
+++ clovis/tables.py
@@ -18,6 +18,7 @@ class Guild:
         }
     )
     create_category_id: int = field(default=None, metadata={'sa': Column(BigInteger)})
+    welcome_channel_id: int = field(default=None, metadata={'sa': Column(BigInteger)})
     last_message_id: int = field(default=None, metadata={'sa': Column(BigInteger)})
     create_channel: bool = field(default=True, metadata={'sa': Column(Boolean, nullable=False)})
     message_error: bool = field(default=True, metadata={'sa': Column(Boolean, nullable=False)})
